d03ece70ac4f7dc69f490487b879bbf1852fe026
Elie Gavoty <eliegavoty@free.fr>
2022-01-17 13:31:49 +0100

    road to fast kubespray

--- hcloud_infra/instances_and_vpc/do_domains.tf
+++ hcloud_infra/instances_and_vpc/do_domains.tf
@@ -10,7 +10,16 @@ resource "digitalocean_record" "workers_subdomains" {
   domain = data.digitalocean_domain.cluster_domain.name
   type   = "A"
   name   = "worker${count.index}.${var.cluster_subdomain}"
-  value  = hcloud_server.control_planes.0.ipv4_address
+  value  = element(hcloud_server.workers.*.ipv4_address, count.index)
+}
+
+
+resource "digitalocean_record" "workers_wildcard_subdomains" {
+  count = length(hcloud_server.workers)
+  domain = data.digitalocean_domain.cluster_domain.name
+  type   = "A"
+  name   = "*.worker${count.index}.${var.cluster_subdomain}"
+  value  = element(hcloud_server.workers.*.ipv4_address, count.index)
 }
 
 resource "digitalocean_record" "control_plane_subdomains" {
@@ -18,21 +27,14 @@ resource "digitalocean_record" "control_plane_subdomains" {
   domain = data.digitalocean_domain.cluster_domain.name
   type   = "A"
   name   = "control-plane${count.index}.${var.cluster_subdomain}"
-  value  = hcloud_server.control_planes.0.ipv4_address
+  value  = element(hcloud_server.control_planes.*.ipv4_address, count.index)
 }
 
-resource "digitalocean_record" "workers_wildcard_subdomains" {
-  count = length(hcloud_server.workers)
-  domain = data.digitalocean_domain.cluster_domain.name
-  type   = "A"
-  name   = "*.worker${count.index}.${var.cluster_subdomain}"
-  value  = hcloud_server.control_planes.0.ipv4_address
-}
 
 resource "digitalocean_record" "control_plane_wildcard_subdomains" {
   count = length(hcloud_server.control_planes)
   domain = data.digitalocean_domain.cluster_domain.name
   type   = "A"
   name   = "*.control-plane${count.index}.${var.cluster_subdomain}"
-  value  = hcloud_server.control_planes.0.ipv4_address
+  value  = element(hcloud_server.control_planes.*.ipv4_address, count.index)
 }
